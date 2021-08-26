31626ee0d8e243bf426ea649aacc131462a1db93
Manpreet <ammdhillon@gmail.com>
2021-08-27 01:54:18 +0530

    second

--- lib/myProfilepage.dart
+++ lib/myProfilepage.dart
@@ -17,128 +17,112 @@ class _MyProfileState extends State<MyProfile> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        backgroundColor: ColorResources.white,
-        title: Text(
-          "My Profile",
-          style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-        ),
-        toolbarHeight: 100,
-        actions: [
-          barButtons("Dashboard", () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => Dashboard()),
-            );
-          }),
-          barButtons("Tree", () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => TreePage()),
-            );
-          }),
-          Padding(
-            padding: EdgeInsets.only(top: 20, bottom: 20, right: 40),
-            child: InkWell(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => TreePage()),
-                );
-              },
-              child: Container(
-                alignment: Alignment.center,
-                margin: EdgeInsets.all(5),
-                width: 120,
-                child: Text(
-                  "Log Out",
-                  style: TextStyle(
-                      fontSize: 20, color: ColorResources.buttonOrange),
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          backgroundColor: ColorResources.white,
+          title: Text(
+            "My Profile",
+            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+          ),
+          toolbarHeight: 100,
+          actions: [
+            barButtons("Dashboard", () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => Dashboard()),
+              );
+            }),
+            barButtons("Tree", () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => TreePage()),
+              );
+            }),
+            Padding(
+              padding: EdgeInsets.only(top: 20, bottom: 20, right: 40),
+              child: InkWell(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => TreePage()),
+                  );
+                },
+                child: Container(
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.all(5),
+                  width: 120,
+                  child: Text(
+                    "Log Out",
+                    style: TextStyle(
+                        fontSize: 20, color: ColorResources.buttonOrange),
+                  ),
+                  decoration: BoxDecoration(
+                      border: Border.all(
+                          color: ColorResources.buttonOrange, width: 2),
+                      borderRadius: BorderRadius.circular(30)),
                 ),
-                decoration: BoxDecoration(
-                    border: Border.all(
-                        color: ColorResources.buttonOrange, width: 2),
-                    borderRadius: BorderRadius.circular(30)),
               ),
             ),
-          ),
-        ],
-      ),
-      body: SingleChildScrollView(
+          ],
+        ),
+        body: ListView(
           padding: EdgeInsets.only(top: 30),
-          child: Row(
-            children: [
-              Image.asset(Images.profile),
-              SizedBox(
-                width: 100,
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  table("Name"),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  table("Mobile No."),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  table("Joining Date"),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  table("Status"),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  table("Level"),
-                ],
-              ),
-              userInfo(1213)
-            ],
-          )),
-    );
+          children: [
+            (Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Image.asset(Images.profile),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    infoColumns("Name", "this is Name"),
+                    infoColumns("Mobile No.", "This is Mobile Number"),
+                    infoColumns("Joining Date", "You Joined on 1/1/2021"),
+                    infoColumns("Status", "Show Status"),
+                    infoColumns("Level", "You are on Level 1")
+                  ],
+                ),
+              ],
+            )),
+          ],
+        ));
   }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-  Widget table(String rowName) {
-    return Container(
-        alignment: Alignment.center,
-        height: 70,
-        width: 160,
-        color: ColorResources.buttonDarkOrange,
-        child: Text(
-          rowName,
-          style: TextStyle(fontSize: 20, color: ColorResources.white),
-        ));
-  }
-
-  Widget userInfo(var info) {
-    return Container(
-        margin: EdgeInsets.only(top: 2),
-        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-          dataRows("1", Colors.orange.shade300),
-          dataRows("2", Colors.orange.shade400),
-          dataRows("\$40", Colors.orange.shade500),
-          dataRows("\$28", Colors.orange.shade600),
-          dataRows("\$14", Colors.orange.shade700),
-          dataRows("\$12", Colors.orange.shade800),
-          dataRows("Rs. 900", Colors.orange.shade900),
-        ]));
-  }
-
-  Widget dataRows(String rowNam, Color col) {
-    return Container(
-        alignment: Alignment.center,
-        height: 50,
-        width: 160,
-        color: col,
-        child: Text(
-          rowNam,
-          style: TextStyle(fontSize: 20, color: ColorResources.white),
-        ));
+  Widget infoColumns(
+    String title,
+    String about,
+  ) {
+    return Column(
+      children: [
+        Container(
+            width: 480,
+            alignment: Alignment.centerLeft,
+            child: Text(
+              title,
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            )),
+        SizedBox(
+          height: 10,
+        ),
+        Container(
+          alignment: Alignment.centerLeft,
+          width: 500,
+          padding: EdgeInsets.only(top: 15, bottom: 15, left: 10),
+          decoration: BoxDecoration(
+              border: Border.all(color: Colors.black),
+              borderRadius: BorderRadius.circular(5)),
+          child: Text(
+            about,
+            style: TextStyle(fontSize: 20),
+          ),
+        ),
+        SizedBox(
+          height: 20,
+        )
+      ],
+    );
   }
 
   Widget barButtons(String txt, Function() onTap) {
